8010b12cddad7ae9e3876d029c053c337f9c0a13
ibz-01 <ibrahimkhalid923@gmail.com>
2026-04-29 22:43:47 +0500

    Update baseURL for public tunnel access

--- src/api/axiosconfig.js
+++ src/api/axiosconfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // This creates a custom Axios instance
 const apiClient = axios.create({
     // Set your backend base URL here ONCE
-    baseURL: 'http://192.168.1.13:3000/api',
+    baseURL: 'https://olive-bars-repeat.loca.lt/api',
     headers: {
         'Content-Type': 'application/json',
     },
